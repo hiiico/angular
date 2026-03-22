74eaac4bbcc54aff4fd81644700f5a0698af7faf
hiiico <hiiico332@gmail.com>
2026-03-22 15:03:39 +0200

    Commit

--- workshop_components/project-workshop/src/app/features/themes/themes.ts
+++ workshop_components/project-workshop/src/app/features/themes/themes.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ThemeContent } from './theme-content/theme-content';
+import {ThemesList} from './themes-list/themes-list';
 
 
 @Component({
   selector: 'app-themes',
-  imports: [ThemeContent],
+  imports: [ThemesList],
   templateUrl: './themes.html',
   styleUrl: './themes.css',
 })
 export class Themes {
-
 }
